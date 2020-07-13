3f51158ada197dc7285db176420f4ac95a329837
Techit-Kakaew <techit.kakaew@gmail.com>
2020-07-14 00:41:10 +0700

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-6 align-self-center">
+            <img src="../assets/images/tuna_banner@2x.png" class="img-fluid" alt />
+          </div>
+          <div class="col-6 align-self-center">
+            <div class="header-section">
+              <h1 class="header-section__title">TUNA</h1>
+              <span class="header-section__subtitle">SOLID, CHUCK, FLAKE, SHREDDED</span>
+            </div>
+            <div class="description">
+              <p>Pellentesque semper quis neque dictum hendrerit. Sed nulla ipsum, porttitor pharetra tortor in,consequat imperdiet nisi. Phasellus at quam tristique, cursus tellus vitae, convallis neque.</p>
+            </div>
+            <div class="line"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="speicies-section">
+      <div class="container">
+        <div class="header">
+          <h1>SPEICIES</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "../assets/css/home.css";
+</style>
